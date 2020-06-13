77e5b2f1d8af08acaa2b8db4f7160e3c2184bfa0
s21611-pj <56191736+s21611-pj@users.noreply.github.com>
2020-06-13 23:41:05 +0200

    Delete UATicketService.java

--- POJ_7/src/com/zad1/UATicketService.java
+++
@@ -1,8 +0,0 @@
-package com.zad1;
-
-import java.util.Date;
-import java.util.List;
-
-public interface UATicketService {
-    List<UATicketInfo> getTicketInfo (String from, String to, Date when);
-}
