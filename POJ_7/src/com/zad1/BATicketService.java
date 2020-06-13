af40b3942ef13c79c883a51263b829120bcfc6cb
s21611-pj <56191736+s21611-pj@users.noreply.github.com>
2020-06-13 23:40:43 +0200

    Delete BATicketService.java

--- POJ_7/src/com/zad1/BATicketService.java
+++
@@ -1,9 +0,0 @@
-package com.zad1;
-
-import java.util.Date;
-import java.util.List;
-
-public interface BATicketService {
-    List<BATicket> getTicketInfo (Airport from, Airport to, Date when);
-
-}
